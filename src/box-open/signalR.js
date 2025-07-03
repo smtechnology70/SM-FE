73918765db693d2bcca682bc3795eef0f687c1c4
sushil70 <69754620+sushil70@users.noreply.github.com>
2025-07-04 01:21:02 +0530

    web socket added

--- src/box-open/signalR.js
+++ src/box-open/signalR.js
@@ -2,7 +2,7 @@ import * as signalR from "@microsoft/signalr";
 
 export const buildConnection = () =>
   new signalR.HubConnectionBuilder()
-    .withUrl("http://localhost:5283/zero-blast", {
+    .withUrl("http://localhost:5027/zero-blast", {
       withCredentials: true,
     }) // adjust for prod
     .withAutomaticReconnect()
