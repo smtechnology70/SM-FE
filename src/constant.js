bc892cfccc8d115f66aeb17e681efd81dbe455f6
sushil70 <sm.technology.70@gmail.com>
2025-07-23 14:05:19 +0530

    backend url changed

--- src/constant.js
+++ src/constant.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = "http://20.197.1.173:5000/api";
-export const WS_BASE_URL = "http://20.197.1.173:5000";
+export const API_BASE_URL = "/api";
+export const WS_BASE_URL = "http://20.193.250.87:5000";
