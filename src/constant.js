8a65bd2fae67b31ae4815ca10d7b860980f14a2d
sushil70 <sm.technology.70@gmail.com>
2025-07-24 19:24:59 +0530

    api cahnged

--- src/constant.js
+++ src/constant.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = "http://localhost:5179/api";
-export const WS_BASE_URL = "http://localhost:5179";
+export const API_BASE_URL = "/api";
+export const WS_BASE_URL = "http://20.193.250.87:5000";
